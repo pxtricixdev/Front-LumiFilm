513b59d41544e2493d6712b2cca2b881b880092e
Patricia <patricia.abadaranda@gmail.com>
2024-11-11 20:43:37 +0100

    Añadido gulp para compilar sass

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+let gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+gulp.task('compilar-sass', function() {
+    gulp.src('source/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('dist/style.css'))
+});
