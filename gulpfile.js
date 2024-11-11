12bed57db3bc1cfeb0b4937c04f76b093902ecea
Patricia <patricia.abadaranda@gmail.com>
2024-11-11 20:44:55 +0100

    Cambio de ruta al compilar sass con gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -4,5 +4,5 @@ const sass = require('gulp-sass')(require('sass'));
 gulp.task('compilar-sass', function() {
     gulp.src('source/main.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('dist/style.css'))
+    .pipe(gulp.dest('dist/style'))
 });
