cb3b891fc5b4e57d9b1d1100aaa3f674e9dc6a97
Patricia <patricia.abadaranda@gmail.com>
2024-11-12 18:16:47 +0100

    Navbar añadido, gulp watch instalado y ajustes de reset.scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,13 @@
 let gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const watch = require("gulp-watch")
 
 gulp.task('compilar-sass', function() {
-    gulp.src('source/main.scss')
+    return gulp.src('source/main.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('dist/style'))
+});
+
+gulp.task( 'watch', function() {
+    gulp.watch( 'source/**/*.scss', gulp.series('compilar-sass'));
 });
