d33101f20cbd3c27ecb7f0b42fc73a5d110a62ea
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-11-14 17:39:24 +0100

    Print pelis en pantalla cartelera

--- scripts/fetchPeliculas.js
+++ scripts/fetchPeliculas.js
@@ -16,60 +16,53 @@ const printMovies = (movies) => {
     const moviesGrid = document.getElementById('movies'); 
 
     movies.forEach(movie => {
-        const { Titulo,  Duracion, Clasificacion, Genero, Imagen } = movie;
+        const { 
+            titulo,  
+            duracion, 
+            clasificacion, 
+            genero, 
+            imagen 
+        } = movie;
 
         const movieCard = document.createElement('div');
         movieCard.classList.add('movie-card');
 
-      
         const movieImage = document.createElement('img');
-        movieImage.src = `../assets/images/pelis/${Imagen}`;
+        movieImage.src = `${imagen}`;
         movieImage.classList.add('movies__imagen');
         movieCard.appendChild(movieImage);
         
         const movieContentRight = document.createElement('div');
         movieContentRight.classList.add('movies__content__right');
 
-       
         const movieTitle = document.createElement('h2');
-        movieTitle.textContent = Titulo;
+        movieTitle.innerText = titulo;
         movieTitle.classList.add('movies__title');
         movieContentRight.appendChild(movieTitle);
-
        
         const movieDuration = document.createElement('div');
         movieDuration.classList.add('movies__duration');
         movieDuration.innerHTML = `
-        <img src="../assets/icons/butaca.svg" alt="Duración"> ${Duracion} min.
+        <img src="../assets/icons/butaca.svg" alt="Duración"> ${duracion} min.
         `;
         movieContentRight.appendChild(movieDuration);
-
         
         const movieCategory = document.createElement('div');
         movieCategory.classList.add('movies__category');
         movieCategory.innerHTML = `
-        <img src="../assets/icons/butaca.svg" alt="Genero"> ${Genero}
+        <img src="../assets/icons/butaca.svg" alt="Genero"> ${genero}
         `;
         movieContentRight.appendChild(movieCategory);
-
         
         const moviePegi = document.createElement('div');
         moviePegi.classList.add('movies__pegi');
         moviePegi.innerHTML = `
-        <img src="../assets/icons/butaca.svg" alt="Clasificación"> ${Clasificacion}
+        <img src="../assets/icons/butaca.svg" alt="Clasificación"> ${clasificacion}
         `;
 
         movieContentRight.appendChild(moviePegi);
-
-        
         movieCard.appendChild(movieContentRight);
-
-
         moviesGrid.appendChild(movieCard);
     });
 };
-
-
-
-
 fetchPeliculas();
