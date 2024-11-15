2daa8673a790485670152ba333501e098553f427
Alex <ianaalexandru03@gmail.com>
2024-11-15 10:03:25 +0100

    Cartelera Listo

--- scripts/fetchPeliculas.js
+++ scripts/fetchPeliculas.js
@@ -1,18 +1,16 @@
-const api_pelis = 'https://localhost:7024/api/Pelicula'
+const api_pelis = 'https://localhost:7024/api/Pelicula';
 
-const fetchPeliculas = async()=> {
+const fetchPeliculas = async () => {
     try {
         const response = await fetch(api_pelis);
         const movies = await response.json();
-        console.log(movies)
+        console.log(movies);
         printMovies(movies);
+    } catch (error) {
+        console.log("Error fetching data ", error);
     }
-    catch(error) {
-        console.log("Error fetching data ",error);
-    }
-}
+};
 
- 
 const clasificacionEdad = (clasificacion) => {
     let rutaImagenClasificacion = '';
     switch (clasificacion) {
@@ -29,61 +27,63 @@ const clasificacionEdad = (clasificacion) => {
             rutaImagenClasificacion = '../assets/icons/edad18.svg';
             break;
     }
-}
-
-const moviesGrid = document.getElementById('movies'); 
-const moviesList = document.getElementById('moviesList')
+    return `<img src="${rutaImagenClasificacion}" alt="Clasificación ${clasificacion}">`;
+};
 
 const printMovies = (movies) => {
 
+    const moviesList = document.getElementById('moviesList'); 
+
     movies.forEach(movie => {
         const { 
-            titulo,  
-            duracion, 
-            clasificacion, 
-            genero, 
-            imagen 
-        } = movie;
-        
-        const movieCard = document.createElement('li');
-        movieCard.classList.add('movie-card');
-        moviesList.appendChild(movieCard);
+            titulo,
+             duracion, 
+             clasificacion,
+              genero, 
+              imagen 
+            } = movie;
+
+        const movieItem = document.createElement('li');
+        movieItem.classList.add('movie-card');
 
         const movieImage = document.createElement('img');
-        movieImage.src = `${imagen}`;
+        movieImage.src = imagen;
         movieImage.classList.add('movies__imagen');
-        movieCard.appendChild(movieImage);
-        
-        const movieContentRight = document.createElement('div');
-        movieContentRight.classList.add('movies__content__right');
+        movieItem.appendChild(movieImage);
+
+        const movieContent = document.createElement('div');
+        movieContent.classList.add('movies__content');
 
         const movieTitle = document.createElement('h2');
-        movieTitle.innerText = titulo;
+        movieTitle.textContent = titulo;
         movieTitle.classList.add('movies__title');
-        movieContentRight.appendChild(movieTitle);
-       
+        movieContent.appendChild(movieTitle);
+
         const movieDuration = document.createElement('div');
         movieDuration.classList.add('movies__duration');
         movieDuration.innerHTML = `
-        <img src="../assets/icons/butaca.svg" alt="Duración"> ${duracion} min.
+            <img src="../assets/icons/reloj.svg" alt="Duración">
+            <span class="movies__value">${duracion}</span>
+            <span class="movies__value">min</span>
         `;
-        movieContentRight.appendChild(movieDuration);
-        
+        movieContent.appendChild(movieDuration);
+
         const movieCategory = document.createElement('div');
         movieCategory.classList.add('movies__category');
         movieCategory.innerHTML = `
-        <img src="../assets/icons/butaca.svg" alt="Genero"> ${genero}
+            <img src="../assets/icons/ticket.svg" alt="Genero">
+            <span class="movies__value">${genero}</span>
         `;
-        movieContentRight.appendChild(movieCategory);
-       
+        movieContent.appendChild(movieCategory);
+
         const moviePegi = document.createElement('div');
         moviePegi.classList.add('movies__pegi');
         moviePegi.innerHTML = clasificacionEdad(clasificacion);
-        console.log(clasificacionEdad());
+        movieContent.appendChild(moviePegi);
 
-        movieContentRight.appendChild(moviePegi);
-        movieCard.appendChild(movieContentRight);
-        moviesGrid.appendChild(movieCard);
+        movieItem.appendChild(movieContent);
+        moviesList.appendChild(movieItem);
     });
 };
+
 fetchPeliculas();
